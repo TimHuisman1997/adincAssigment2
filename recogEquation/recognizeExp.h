4fa28de42a446c332369760720f9621dd1f51a4c
Tim Huisman <tim.huisman.1997@gmail.com>
2018-02-15 18:19:18 +0100

    added recognize.h

---
+++ recogEquation/recognizeExp.h
@@ -0,0 +1,13 @@
+/* recognizeExp.h, Gerard Renardel, 29 January 2014 */
+
+#ifndef RECOGNIZEEXP_H
+#define RECOGNIZEEXP_H
+#include "scanner.h"
+
+int acceptNumber(List *lp);
+int acceptIdentifier(List *lp);
+int acceptCharacter(List *lp, char c);
+int acceptExpression(List *lp);
+void recognizeExpressions();
+
+#endif
