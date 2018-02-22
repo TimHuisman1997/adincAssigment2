c36a8afc13cfbff70626809e7d91e298abd25ab1
TimHuisman1997 <tim.huisman.1997@gmail.com>
2018-02-22 16:01:29 +0100

    add

--- recogEquation/recognizeEquation.h
+++ recogEquation/recognizeEquation.h
@@ -4,11 +4,11 @@
 #define RECOGNIZEEXP_H
 #include "scanner.h"
 
-int acceptNumber(List *lp);
+int acceptNumber(List *lp, int *temp);
 int acceptIdentifier(List *lp, int *var, char *s);
-int acceptCharacter(List *lp, char c);
-int acceptExpression(List *lp, int *deg, int *var, char *s);
-int acceptEquation(List *lp, int *deg, int *var, char *s);
+int acceptCharacter(List *lp, char c, int *plusMin, int *equal);
+int acceptExpression(List *lp, int *deg, int *var, char *s, double *arAB, int *plusMin, int *equal);
+int acceptEquation(List *lp, int *deg, int *var, char *s, double *arAB, int *plusMin, int *equal);
 void recognizeEquation();
 
 #endif
